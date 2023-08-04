06a26a937f458a8acc748f04f81d6a37e60a1fc3
enes polat <enespolat@eness-MacBook-Pro-2.local>
2023-08-04 15:47:13 +0300

    htmlde goruntule

--- cardcollector.js
+++ cardcollector.js
@@ -1,10 +1,26 @@
-async function loadCards(){
-    try{
+async function loadCards() {
+    let cardTable = document.getElementById("myCards");
+    try {
         const response = await fetch('http://127.0.0.1:5500/cards.json');
         let cardsJson = await response.json();
-        console.log(cardsJson);
+        
+
+        var cards = cardsJson.map(function (card) {
+            var cardsToShow = {
+                "id": card.id,
+                "name": card.name,
+                "price": card.priceUSD,
+                "isRare": card.isRare
+            }
+            return cardsToShow;
+        })
     }
-    catch(err){
+    catch (err) {
         alert(err);
     }
+    cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
+    for (var i = 0; i < cards.length; i++) {
+        cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + cards[i].price + "</td><td>" + cards[i].isRare + "</td></tr>";
+    }
 }
+
