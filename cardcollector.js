79c0da7ae25e378b344132d44d6cfb77b8a322cf
enes polat <enespolat@eness-MacBook-Pro-2.local>
2023-08-04 16:02:06 +0300

    filtre

--- cardcollector.js
+++ cardcollector.js
@@ -1,5 +1,6 @@
 async function loadCards() {
     let cardTable = document.getElementById("myCards");
+    
     try {
         const response = await fetch('http://127.0.0.1:5500/cards.json');
         let cardsJson = await response.json();
@@ -9,7 +10,7 @@ async function loadCards() {
             var cardsToShow = {
                 "id": card.id,
                 "name": card.name,
-                "price": card.priceUSD,
+                "priceUSD": card.priceUSD,
                 "isRare": card.isRare
             }
             return cardsToShow;
@@ -19,8 +20,19 @@ async function loadCards() {
         alert(err);
     }
     cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
-    for (var i = 0; i < cards.length; i++) {
-        cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + cards[i].price + "</td><td>" + cards[i].isRare + "</td></tr>";
+    let rareCards = cards.filter(obj => obj.isRare == true).map(obj => ({"id":obj.id, "name":obj.name, "priceUSD":obj.priceUSD,"isRare":obj.isRare}));
+    
+
+    let onlyRares = document.getElementById("raresOnly");
+
+    if(onlyRares.checked){
+        for (var i = 0; i < rareCards.length; i++){
+            cardTable.innerHTML += "<tr><td>" + rareCards[i].id + "</td><td>" + rareCards[i].name + "</td><td>" + rareCards[i].priceUSD + "</td><td>" + rareCards[i].isRare + "</td></tr>";
+        }
+    }else{
+        for (var i = 0; i < cards.length; i++){
+            cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + cards[i].priceUSD + "</td><td>" + cards[i].isRare + "</td></tr>";
+        }
     }
 }
 
