635c16615e9c266e543ed2f475884db7dbe12b37
enes polat <enespolat@eness-MacBook-Pro-2.local>
2023-08-04 16:07:36 +0300

    euro dolar donusumu

--- cardcollector.js
+++ cardcollector.js
@@ -1,10 +1,19 @@
 async function loadCards() {
     let cardTable = document.getElementById("myCards");
-    
+    let inEuro = document.getElementById("currency");
+    const EURO_CONVERSION = 0.87;
+    let priceMultiplier;
+
+    if(inEuro.checked){
+        priceMultiplier = EURO_CONVERSION;
+    } else{
+        priceMultiplier = 1;
+    }
+
     try {
         const response = await fetch('http://127.0.0.1:5500/cards.json');
         let cardsJson = await response.json();
-        
+
 
         var cards = cardsJson.map(function (card) {
             var cardsToShow = {
@@ -20,18 +29,18 @@ async function loadCards() {
         alert(err);
     }
     cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
-    let rareCards = cards.filter(obj => obj.isRare == true).map(obj => ({"id":obj.id, "name":obj.name, "priceUSD":obj.priceUSD,"isRare":obj.isRare}));
-    
+    let rareCards = cards.filter(obj => obj.isRare == true).map(obj => ({ "id": obj.id, "name": obj.name, "priceUSD": obj.priceUSD, "isRare": obj.isRare }));
+
 
     let onlyRares = document.getElementById("raresOnly");
 
-    if(onlyRares.checked){
-        for (var i = 0; i < rareCards.length; i++){
-            cardTable.innerHTML += "<tr><td>" + rareCards[i].id + "</td><td>" + rareCards[i].name + "</td><td>" + rareCards[i].priceUSD + "</td><td>" + rareCards[i].isRare + "</td></tr>";
+    if (onlyRares.checked) {
+        for (var i = 0; i < rareCards.length; i++) {
+            cardTable.innerHTML += "<tr><td>" + rareCards[i].id + "</td><td>" + rareCards[i].name + "</td><td>" + Math.round(rareCards[i].priceUSD * priceMultiplier) + "</td><td>" + rareCards[i].isRare + "</td></tr>";
         }
-    }else{
-        for (var i = 0; i < cards.length; i++){
-            cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + cards[i].priceUSD + "</td><td>" + cards[i].isRare + "</td></tr>";
+    } else {
+        for (var i = 0; i < cards.length; i++) {
+            cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + Math.round(cards[i].priceUSD * priceMultiplier) + "</td><td>" + cards[i].isRare + "</td></tr>";
         }
     }
 }
